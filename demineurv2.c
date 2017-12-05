47aeb1d3ef6fbebfc2a7fdbf88b397b8881a3ca5
LeaLB <lea.le-bouedec@etu.univ-orleans.fr>
2017-12-05 14:43:53 +0100

    Mise en forme, simplification, utilisation de la fonction init

--- demineurv2.c
+++ demineurv2.c
@@ -5,11 +5,79 @@
 
 static int tabstock[7][7] ;
 
-// Fonctions
-// Fonction Affichage
+    //______________FONCTIONS______________
+
+void init(int taille,int tabAffichage[7][7],int tabValeurs[7][7], int nb_mines_placee, int cpt){
+
+    //___Initialisation des valeurs
+    int val1, val2 ;
+    //___Initialisation des tableaux
+    for (int i = 0 ; i<taille ; i++){
+        for (int j = 0 ; j<taille ; j++){
+            tabAffichage[i][j] = -1 ;
+            tabValeurs[i][j] = 0 ;
+            if (0<i && i<6){
+               if (0<j && j<6){
+                   tabstock[i][j] = -1 ;
+               }
+               else{
+                   tabstock[i][j] = -2 ;
+               }
+           }
+           else {
+               tabstock[i][j] = -2 ;
+           }
+        }
+    }
+
+    //___Initialisation des mines
+    srand(time(NULL));
+    while (nb_mines_placee < 5){
+        val1 = 1+rand()%5 ;
+        val2 = 1+rand()%5 ;
+        if (tabValeurs[val1][val1]!=9){
+            tabValeurs[val1][val2] = 9 ;
+            nb_mines_placee++;
+        }
+    }
+
+    //___Calcul du nombre de mines voisines
+    for (int i=1 ; i<(taille-1) ; i++){
+        for (int j=1 ; j<(taille-1) ; j++){
+            cpt = 0;
+            if (tabValeurs[i][j]!=9){
+                if (tabValeurs[i-1][j-1]==9){
+                    cpt++;
+                }
+                if (tabValeurs[i-1][j]==9){
+                    cpt++;
+                }
+                if (tabValeurs[i-1][j+1]==9){
+                    cpt++;
+                }
+                if (tabValeurs[i][j-1]==9){
+                    cpt++;
+                }
+                if (tabValeurs[i][j+1]==9){
+                    cpt++;
+                }
+                if (tabValeurs[i+1][j-1]==9){
+                    cpt++;
+                }
+                if (tabValeurs[i+1][j]==9){
+                    cpt++;
+                }
+                if (tabValeurs[i+1][j+1]==9){
+                    cpt++;
+                }
+                tabValeurs[i][j] = cpt ;
+            }
+        }
+    }
+}
 
 void affichage(int tab[7][7], int taille){
-    //system("clear");
+    system("clear");
     for (int i = 1 ; i < taille-1 ; i++){
         for(int j = 1 ; j < taille-1 ; j++){
             if (tab[i][j]==-1){
@@ -97,82 +165,17 @@ int nb_case_decouverte(int tabaff[7][7], int taille){
 }
 
 int main(){
-    //Initialisation des valeurs
+
+    //______________INITIALISATION_________________
+
     int taille = 7, nb_mines = 5, continuer = 1, nb_case_dec ;
-    int ligne = 0, colonne = 0, cpt = 0, val1, val2, nb_mines_placee = 0;
+    int ligne = 0, colonne = 0, cpt = 0, nb_mines_placee = 0;
     int tabAffichage [taille][taille], tabValeurs[taille][taille];
 
-    // Programme
+    init(taille,tabAffichage,tabValeurs, nb_mines_placee, cpt);
 
-    // Initialisation des tableaux
+    //______________PROGRAMMATION__________________
 
-    for (int i = 0 ; i<taille ; i++){
-        for (int j = 0 ; j<taille ; j++){
-            tabAffichage[i][j] = -1 ;
-            tabValeurs[i][j] = 0 ;
-            if (0<i && i<6){
-               if (0<j && j<6){
-                   tabstock[i][j] = -1 ;
-               }
-               else{
-                   tabstock[i][j] = -2 ;
-               }
-           }
-           else {
-               tabstock[i][j] = -2 ;
-           }
-        }
-    }
-
-    // Mise en place des mines
-    // la mine aura la valeur 9
-    srand(time(NULL));
-    while (nb_mines_placee < 5){
-        val1 = 1+rand()%5 ;
-        val2 = 1+rand()%5 ;
-        if (tabValeurs[val1][val1]!=9){
-            tabValeurs[val1][val2] = 9 ;
-            nb_mines_placee++;
-        }
-    }
-
-    // Calcul du nombre de mines voisines
-    for (int i=1 ; i<(taille-1) ; i++){
-        for (int j=1 ; j<(taille-1) ; j++){
-            cpt = 0;
-            if (tabValeurs[i][j]!=9){
-                if (tabValeurs[i-1][j-1]==9){
-                    cpt++;
-                }
-                if (tabValeurs[i-1][j]==9){
-                    cpt++;
-                }
-                if (tabValeurs[i-1][j+1]==9){
-                    cpt++;
-                }
-                if (tabValeurs[i][j-1]==9){
-                    cpt++;
-                }
-                if (tabValeurs[i][j+1]==9){
-                    cpt++;
-                }
-                if (tabValeurs[i+1][j-1]==9){
-                    cpt++;
-                }
-                if (tabValeurs[i+1][j]==9){
-                    cpt++;
-                }
-                if (tabValeurs[i+1][j+1]==9){
-                    cpt++;
-                }
-                tabValeurs[i][j] = cpt ;
-            }
-        }
-    }
-
-    // Jeu
-
-    affichage(tabValeurs,taille);
     while (continuer == 1){
         ligne = -1 ;
         while (ligne < 0 || taille-2 < ligne){
@@ -185,7 +188,7 @@ int main(){
             scanf("%d",&colonne);
         }
 
-        //Si je tombe sur une mine
+        // Si je tombe sur une mine
         if (tabValeurs[ligne][colonne] == 9){
             continuer = 0 ;
             tabAffichage[ligne][colonne] = 9 ;
@@ -193,7 +196,7 @@ int main(){
             printf("Vous avez perdu\n");
         }
 
-        //Si je ne tombe pas sur une mine
+        // Si je ne tombe pas sur une mine
         else{
             if (tabAffichage[ligne][colonne] == -1 ){
                 tabAffichage[ligne][colonne] = tabValeurs[ligne][colonne] ;
